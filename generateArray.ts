1e0d8119673429fe8160508fc019f99eb7e24d1a
Aswin Reji <aswinreji.004@gmail.com>
2024-10-12 18:58:12 +0000

    feat: Recursion

--- generateArray.ts
+++ generateArray.ts
@@ -1,24 +1,40 @@
-function* generator(num:number) {
- yield num;
- yield num + 10;
+//function* generator(num:number) {
+// yield num;
+// yield num + 10;
+//}
+//
+//const gen = generator(10)
+//
+//console.log(gen.next().value);
+//console.log(gen.next().value);
+//
+//
+//function* yieldAndReturn() {
+//  yield "Y";
+//  yield "$Hello"
+//  return "R";
+//  yield "unreacheable";
+//}
+//
+//const gene = yieldAndReturn()
+//
+//console.log(gene.next());
+//console.log(gene.next());
+//console.log(gene.next());
+//console.log(gene.next());
+//
+//
+function* powers(n:number) {
+  
+  for(let current = n; ; current *= n){
+    yield current;
+  }
 }
 
-const gen = generator(10)
-
-console.log(gen.next().value);
-console.log(gen.next().value);
-
-
-function* yieldAndReturn() {
-  yield "Y";
-  return "R";
-  yield "unreacheable";
+for (const power of powers(2)) {
+  if(power > 32) {
+    break
+  }
+  console.log(power);
+  
 }
-
-const gene = yieldAndReturn()
-
-console.log(gene.next());
-console.log(gene.next());
-console.log(gene.next());
-
-
---
+++ recursion.ts
@@ -0,0 +1,11 @@
+function recursion(n) {
+  if(n === 100) {
+    return "hello world"
+  }
+  n++
+  return recursion(n)
+}
+
+const data = recursion(1)
+console.log(data,'data is showing');
+
