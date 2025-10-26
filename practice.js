f9fe58bd0091fa7f9c0f4dab0f6ae1083a8a746a
Aswin Reji <aswinreji.004@gmail.com>
2025-10-26 17:17:17 +0000

    feat: learned block scope , shadowing let const specilality tdz

--- practice.js
+++ practice.js
@@ -33,14 +33,46 @@
 // console.log(global.window)
 
 
-function yoo(params) {
- var a = 10; 
-}
-yoo()
-console.log(a,'a')
+// function yoo(params) {
+//  var a = 10; 
+// }
+// yoo()
+// console.log(a,'a')
 
 // window is actually a global object which is created along with the global execution context
 // whenever any js program is run there is global object is created global execution context is created and along with the execution context this variable is created 
 // global object in case of browser is window 
 // wherever js running it has js engine , all this js engine has the repsonsibility to create the global object , in the case of browser it is known as window ,
 // At this global level this === window 
+//
+//
+//block is defined by {} bracked 
+//block is also known as compound statement  /// important
+// we need to use this cuz javascript need multiple statement result as a one statement 
+//so this is combining multiple statements into group 
+//so block is used to combine multiple statement into one group 
+// we need to group these statement together so we can use multiple statement in a place where js expect only one statement 
+//
+// block means what all functions and variables we can access in the scope
+//
+// eg 
+
+// shadowing
+var a = 100;
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  console.log(a,'a')
+  console.log(b,'b')
+  console.log(c,'c')
+}
+  console.log(a,'a')
+  // console.log(b,'b')
+  // console.log(c,'c')
+//
+//
+//
+// Closure
+//
+//
