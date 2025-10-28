b17d0e2f1e9733eb4e171274c43d515a0166d163
Aswin Reji <aswinreji.004@gmail.com>
2025-10-28 11:45:58 +0000

    feat: learning leetcode questions logic

--- leetcode.js
+++ leetcode.js
@@ -1,31 +1,63 @@
-let str = "abcdefghi";
+// let str = "abcdefghi";
+// let str = "a"
+//
+// let k = 3;
+//
+// let count = 0
+// let arr = []
+// let newStr = ""
 
-let k = 3;
+// for (let i =0; i<str.length; i++){
+//   newStr += str[i]
+//     count++
+//   if(count == k)  {
+//     arr.push(newStr)  
+//     count = 0
+//     newStr = ""
+//   }
+// }
 
-let count = 0
+
+// let k = 3
+
+// let fill = "x"
+//
+//
+// for(let i = 0; i<str.length; i++){
+//   newStr += str[i] 
+//   console.log(newStr,'newStr')
+//   if(newStr.length < k){
+//     for (let j = 0; j <= k - newStr.length; j++) {
+//     newStr += fill
+//     }
+//   } 
+//   console.log(newStr,'newStr') 
+// }
+//
+//
+// console.log(arr,'arr')
+//
+//
+let nums = [9,4,1,7];
+let k = 1;
+let subArrays = []
 let arr = []
-let newStr = ""
+let count = 0
 
-for (let i =0; i<str.length; i++){
-  newStr += str[i]
-    count++
-  if(count == k)  {
-    arr.push(newStr)  
-    count = 0
-    newStr = ""
+for (let i = 0; i < nums.length - 1; i++) {
+  for (let j = i; j < nums.length - 1; j++) {
+
+      count++
+      arr.push(nums[j])
+
+    if(count == k) {
+        subArrays.push(arr)
+        arr = [];
+        count = 0
+        break;
+      }
   }
 }
 
-let str = "abcde"
-
-let k = 3
-
-let fill = "x"
-
-
-for(let i = 0; i<str.length; i++){
-  
-}
-
-
-console.log(arr,'arr')
+console.log('arr',arr)
+console.log(subArrays,'newArr is here')
